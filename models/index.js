12c47a649802ce6ff053c6789e0a8f42c3c790a7
Khazz <thelenaid@gmail.com>
2026-08-19 11:00:44 +0700

    index.js

--- models/index.js
+++ models/index.js
@@ -6,29 +6,50 @@ const Sequelize = require('sequelize');
 const process = require('process');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
+const config = require(__dirname + '/../config/config.js')[env];
 const db = {};
 
 let sequelize;
+
 if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+  const connectionUrl = process.env[config.use_env_variable];
+
+  const url = new URL(connectionUrl);
+
+  url.searchParams.delete('sslmode');
+
+  sequelize = new Sequelize(url.toString(), {
+    ...config,
+    dialect: 'postgres',
+    dialectModule: pg,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
+  });
 } else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
+  sequelize = new Sequelize(
+    config.database,
+    config.username,
+    config.password,
+    config
+  );
 }
 
-fs
-  .readdirSync(__dirname)
+fs.readdirSync(__dirname)
   .filter(file => {
     return (
       file.indexOf('.') !== 0 &&
-      file !== basename &&
+      file !== basename && // Jangan me-load index.js itu sendiri
       file.slice(-3) === '.js' &&
       file.indexOf('.test.js') === -1
     );
   })
   .forEach(file => {
     const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
-    db[model.name] = model;
+    db[model.name] = model; // Ini lokasi error kamu kalau ada model yang lupa `return`
   });
 
 Object.keys(db).forEach(modelName => {
