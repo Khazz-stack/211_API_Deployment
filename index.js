e5c505e48a389f753967d108632397f32bf4c12c
Khazz <thelenaid@gmail.com>
2026-08-19 11:09:20 +0700

    Mengubah if

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const connectDatabase = require('./config/db.js');
 
 const app = express();
-const PORT = 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
